2b3f37c58c58fc63818c8a21b900a96fd48a35c9
Nick <nickmartin1ee7@gmail.com>
2021-08-05 21:53:41 -0400

    Overloaded stutter chance

--- Uwuify.Humanizer.Benchmarks/Program.cs
+++ Uwuify.Humanizer.Benchmarks/Program.cs
@@ -16,24 +16,47 @@ namespace Uwuify.Humanizer.Benchmarks
     public class Benchy
     {
         [Benchmark]
-        public void Test1()
+        public void TestDefaultShort()
         {
             "Hit or miss I guess they never miss, huh? You got a boyfriend, I bet he doesn't kiss ya He gon' find another girl and he won't miss ya He gon' skrrt and hit the dab like Wiz Khalifa."
                 .Uwuify();
         }
 
         [Benchmark]
-        public void Test2()
+        public void TestDefaultMedium()
         {
             "The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start."
                 .Uwuify();
         }
 
         [Benchmark]
-        public void Test3()
+        public void TestDefaultLong()
         {
             "Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It’s not a story the Jedi would tell you. It’s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself."
                 .Uwuify();
+        }
+
+        private readonly double _customStutterChance = 0.6;
+
+        [Benchmark]
+        public void TestCustomShort()
+        {
+            "Hit or miss I guess they never miss, huh? You got a boyfriend, I bet he doesn't kiss ya He gon' find another girl and he won't miss ya He gon' skrrt and hit the dab like Wiz Khalifa."
+                .Uwuify(_customStutterChance);
+        }
+
+        [Benchmark]
+        public void TestCustomMedium()
+        {
+            "The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start."
+                .Uwuify(_customStutterChance);
+        }
+
+        [Benchmark]
+        public void TestCustomLong()
+        {
+            "Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It’s not a story the Jedi would tell you. It’s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself."
+                .Uwuify(_customStutterChance);
         }
     }
 }
--- Uwuify.Humanizer/StringExtensions.cs
+++ Uwuify.Humanizer/StringExtensions.cs
@@ -4,9 +4,22 @@
     {
         private static readonly UwuLogic _UwuLogic = new(0.3);
 
-        public static string Uwuify(this string input)
-        {
-            return _UwuLogic.ConvertToUwuFormat(input);
-        }
+        /// <summary>
+        /// Formats <paramref name="input"/> into the UwU meme format.
+        /// </summary>
+        /// <param name="input">Given input string.</param>
+        /// <returns>A new formatted version of the original string.</returns>
+        public static string Uwuify(this string input) =>
+            _UwuLogic.ConvertToUwuFormat(input);
+
+        /// <summary>
+        /// Formats <paramref name="input"/> into the UwU meme format.
+        /// This overload is slightly slower than the <see cref="Uwuify(string)" /> method.
+        /// </summary>
+        /// <param name="input">Given input string.</param>
+        /// <param name="stutterChance">Define a c-custom s-stutter c-chance.</param>
+        /// <returns>A new formatted version of the original string.</returns>
+        public static string Uwuify(this string input, double stutterChance) =>
+            new UwuLogic(stutterChance).ConvertToUwuFormat(input);
     }
 }
